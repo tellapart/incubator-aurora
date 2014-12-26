145f4a3dbfca59ab72288db69cb9f1e5cdcc6576
steve <steve@tellapart.com>
2014-12-26 15:38:27 -0500

    Cleaned up docker example

--- examples/jobs/docker/thermos_executor.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$1" == "--dockerize" ]; then
-	cd $MESOS_SANDBOX
-	exec $MESOS_SANDBOX/thermos_executor.pex --nosetuid
-else
-	exec /home/vagrant/aurora/dist/thermos_executor.pex
-fi
-
